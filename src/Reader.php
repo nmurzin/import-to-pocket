cabc0c59b4260e15aa52b449dd20fb301af17e66
Nikita Murzin <r0s0m4xa@gmail.com>
2018-11-05 18:40:02 +0200

    Finished reader

--- src/Reader.php
+++ src/Reader.php
@@ -10,14 +10,24 @@ class Reader
         $this->fileName = $fileName;
     }
 
-    public function readFile()
+    public function readFile():array
     {
         $opened = fopen('/var/www/'. $this->fileName, 'r');
-        while(! feof($opened))
+        $result = [];
+        while(!feof($opened))
         {
-            echo fgets($opened). "<br />";
+			$result = array_merge($result, $this->parseString(fgets($opened)));
         }
 
         fclose($opened);
+
+		return $result;
     }
+
+	private function parseString(string $string): array {
+    	$string_exploded = explode('|', $string);
+    	if(!empty($string_exploded)){
+			return [trim($string_exploded[0]) => trim($string_exploded[1])];
+		}
+	}
 }
