2b440dbb0c569b36bba5d9913e442be87fd6c786
Nikita Murzin <r0s0m4xa@gmail.com>
2018-11-13 18:40:36 +0200

    Added importer

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ $readerResult = (new \ImportToPocket\Reader(getenv('SOURCE')))->readFile();
 $auth = new \ImportToPocket\Auth();
 
 if ((new \ImportToPocket\Console())->exec($auth->authorize())) {
-    new \ImportToPocket\Importer($readerResult, $auth->authenticate());
+    new \ImportToPocket\Importer($readerResult, $auth->authenticate(), getenv('POCKET_CONSUMER_KEY'));
 }
 
--- src/Importer.php
+++ src/Importer.php
@@ -2,14 +2,48 @@
 
 namespace ImportToPocket;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
 class Importer
 {
+    private $client;
     private $readerResult;
     private $accessToken;
+    private $consumerKey;
 
-    public function __construct(array $readerResult, string $accessToken)
+    public function __construct(array $readerResult, string $accessToken, string $consumerKey)
     {
         $this->readerResult = $readerResult;
         $this->accessToken = $accessToken;
+        $this->consumerKey= $consumerKey;
+		$this->client = new Client();
     }
+
+    public function add()
+	{
+		if ( ! empty( $this->readerResult ) ) {
+			foreach ( $this->readerResult as $url => $title ) {
+				$response = $this->client->send( $this->prepareRequest( $url, $title ) );
+				if ( 200 !== $response->getStatusCode() ) {
+					throw new \Exception( 'Request failed: Status code: ' . $response->getStatusCode() );
+				}
+			}
+		}
+	}
+
+	private function prepareRequest( string $url, string $title ): object
+	{
+		$uri     = 'https://getpocket.com/v3/add';
+		$headers = [ 'Content-Type' => 'application/json; charset=UTF8', 'X-Accept' => 'application/json' ];
+		$body    = [
+			'url' => $url,
+			'title' => $title,
+			'consumer_key' => $this->consumerKey,
+			'access_token' => $this->accessToken,
+		];
+
+		return new Request( 'POST', $uri, $headers, json_encode( $body ) );
+
+	}
 }
